d53614d150d570bf9304be1cb329ce0d74f96e42
bakjoul <bakjoul@gmail.com>
2022-01-31 13:43:02 +0100

    Created MeetingListViewState

---
+++ app/src/main/java/com/bakjoul/mareu/ui/MeetingListViewState.java
@@ -0,0 +1,33 @@
+package com.bakjoul.mareu.ui;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+import java.util.Objects;
+
+public class MeetingListViewState {
+    @NonNull
+    private final List<MeetingItemViewState> meetingItemViewStateList;
+
+    public MeetingListViewState(@NonNull List<MeetingItemViewState> meetingItemViewStateList) {
+        this.meetingItemViewStateList = meetingItemViewStateList;
+    }
+
+    @NonNull
+    public List<MeetingItemViewState> getMeetingItemViewStateList() {
+        return meetingItemViewStateList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MeetingListViewState that = (MeetingListViewState) o;
+        return meetingItemViewStateList.equals(that.meetingItemViewStateList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(meetingItemViewStateList);
+    }
+}
