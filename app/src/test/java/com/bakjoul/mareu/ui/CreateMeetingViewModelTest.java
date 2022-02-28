f0b6eecef87d74feeee086614d17a3c97f9a9b4a
bakjoul <bakjoul@gmail.com>
2022-02-28 20:33:15 +0100

    Created CreateMeetingViewModelTest

---
+++ app/src/test/java/com/bakjoul/mareu/ui/CreateMeetingViewModelTest.java
@@ -0,0 +1,27 @@
+package com.bakjoul.mareu.ui;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+
+import com.bakjoul.mareu.data.repository.MeetingRepository;
+import com.bakjoul.mareu.ui.create.CreateMeetingViewModel;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.mockito.Mock;
+
+public class CreateMeetingViewModelTest {
+
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+    @Mock
+    private MeetingRepository meetingRepository;
+
+    private CreateMeetingViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new CreateMeetingViewModel(meetingRepository);
+    }
+
+}
