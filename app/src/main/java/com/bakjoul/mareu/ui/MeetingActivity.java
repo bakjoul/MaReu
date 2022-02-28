9ae78925e26a4dfcb731497e4e932b35cda4ff8c
bakjoul <bakjoul@gmail.com>
2022-02-28 22:44:38 +0100

    Refactored the way dialogs are shown, prevented multiple instance of the same dialog

--- app/src/main/java/com/bakjoul/mareu/ui/MeetingActivity.java
+++ app/src/main/java/com/bakjoul/mareu/ui/MeetingActivity.java
@@ -1,6 +1,8 @@
 package com.bakjoul.mareu.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,6 +22,10 @@ import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class MeetingActivity extends AppCompatActivity implements OnDeleteClickedListener {
+
+    private final String TAG_CREATE_DIALOG = "CREATE";
+    private final String TAG_ROOM_DIALOG = "ROOM";
+    private final String TAG_DATE_DIALOG = "DATE";
 
     private MeetingActivityBinding b;
     private MeetingViewModel viewModel;
@@ -78,12 +84,31 @@ public class MeetingActivity extends AppCompatActivity implements OnDeleteClicke
     // Observe le résultat des clics sur les boutons et ouvre le dialog adéquat
     private void initDialogs() {
         viewModel.getSingleLiveEvent().observe(this, viewEvent -> {
-            if (viewEvent == MeetingViewEvent.DISPLAY_CREATE_MEETING_DIALOG)
-                CreateMeetingDialogFragment.newInstance().show(getSupportFragmentManager(), "create");
-            else if (viewEvent == MeetingViewEvent.DISPLAY_DATE_FILTER_DIALOG)
-                DateFilterDialogFragment.newInstance().show(getSupportFragmentManager(), "date");
-            else if (viewEvent == MeetingViewEvent.DISPLAY_ROOM_FILTER_DIALOG)
-                RoomFilterDialogFragment.newInstance().show(getSupportFragmentManager(), "room");
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            Fragment prev;
+            switch (viewEvent) {
+                case DISPLAY_CREATE_MEETING_DIALOG:
+                    prev = getSupportFragmentManager().findFragmentByTag(TAG_CREATE_DIALOG);
+                    if (prev == null) {
+                        ft.addToBackStack(null);
+                        CreateMeetingDialogFragment.newInstance().show(ft, TAG_CREATE_DIALOG);
+                    }
+                    break;
+                case DISPLAY_DATE_FILTER_DIALOG:
+                    prev = getSupportFragmentManager().findFragmentByTag(TAG_DATE_DIALOG);
+                    if (prev == null) {
+                        ft.addToBackStack(null);
+                        DateFilterDialogFragment.newInstance().show(getSupportFragmentManager(), TAG_DATE_DIALOG);
+                    }
+                    break;
+                case DISPLAY_ROOM_FILTER_DIALOG:
+                    prev = getSupportFragmentManager().findFragmentByTag(TAG_ROOM_DIALOG);
+                    if (prev == null) {
+                        ft.addToBackStack(null);
+                        RoomFilterDialogFragment.newInstance().show(getSupportFragmentManager(), TAG_ROOM_DIALOG);
+                    }
+                    break;
+            }
         });
     }
 
