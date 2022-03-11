4aee3eae9fea36ab0141c43efcd27dd35eefd01a
bakjoul <bakjoul@gmail.com>
2022-03-11 14:22:37 +0100

    Added tests to DateFilterViewModelTest

--- app/src/test/java/com/bakjoul/mareu/ui/date_filter/DateFilterViewModelTest.java
+++ app/src/test/java/com/bakjoul/mareu/ui/date_filter/DateFilterViewModelTest.java
@@ -1,5 +1,6 @@
 package com.bakjoul.mareu.ui.date_filter;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.BDDMockito.given;
 
 import androidx.annotation.NonNull;
@@ -8,6 +9,7 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.lifecycle.MutableLiveData;
 
 import com.bakjoul.mareu.data.repository.FilterParametersRepository;
+import com.bakjoul.mareu.ui.MeetingViewEvent;
 import com.bakjoul.mareu.utils.LiveDataTestUtil;
 
 import org.junit.Before;
@@ -22,14 +24,12 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
-import static org.junit.Assert.assertEquals;
-
 @RunWith(MockitoJUnitRunner.class)
 public class DateFilterViewModelTest {
 
-    private static final LocalDate DATE = LocalDate.of(2027, 2, 22);
-    private static final LocalTime START_TIME = LocalTime.of(10,0);
-    private static final LocalTime END_TIME = LocalTime.of(11,0);
+    private static final LocalDate DEFAULT_DATE = LocalDate.of(2027, 2, 22);
+    private static final LocalTime DEFAULT_START_TIME = LocalTime.of(10, 0);
+    private static final LocalTime DEFAULT_END_TIME = LocalTime.of(11, 0);
 
     @Rule
     public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
@@ -65,22 +65,132 @@ public class DateFilterViewModelTest {
         DateFilterViewState result = LiveDataTestUtil.getValueForTesting(viewModel.getViewStateMutableLiveData());
 
         // Then
-        assertEquals(getExpectedDateFilterViewState(null, null , null), result);
+        assertEquals(getExpectedDateFilterViewState(null, null, null), result);
     }
 
-    // Vérifie que si on remplit les champs alors la livedata expose le   avec les valeurs saisies
     @Test
-    public void given_all_inputs_are_filled_livedata_should_expose_the_inputted_values() {
+    public void given_on_date_picker_clicked_single_live_event_should_expose_display_date_picker_view_event() {
         // Given
-        viewModel.onDateChanged(DATE.getYear(), DATE.getMonthValue(), DATE.getDayOfMonth());
-        viewModel.onStartTimeChanged(START_TIME.getHour(), START_TIME.getMinute());
-        viewModel.onEndTimeChanged(END_TIME.getHour(), END_TIME.getMinute());
+        viewModel.onDisplayDatePickerClicked();
+
+        // When
+        MeetingViewEvent result = LiveDataTestUtil.getValueForTesting(viewModel.getSingleLiveEvent());
+
+        // Then
+        assertEquals(MeetingViewEvent.DISPLAY_CREATE_MEETING_DATE_PICKER, result);
+    }
+
+    @Test
+    public void given_on_start_picker_clicked_single_live_event_should_expose_display_start_picker_view_event() {
+        // Given
+        viewModel.onDisplayStartTimePickerClicked();
+
+        // When
+        MeetingViewEvent result = LiveDataTestUtil.getValueForTesting(viewModel.getSingleLiveEvent());
+
+        // Then
+        assertEquals(MeetingViewEvent.DISPLAY_CREATE_MEETING_START_PICKER, result);
+    }
+
+    @Test
+    public void given_on_end_picker_clicked_single_live_event_should_expose_display_end_picker_view_event() {
+        // Given
+        viewModel.onDisplayEndTimePickerClicked();
+
+        // When
+        MeetingViewEvent result = LiveDataTestUtil.getValueForTesting(viewModel.getSingleLiveEvent());
+
+        // Then
+        assertEquals(MeetingViewEvent.DISPLAY_CREATE_MEETING_END_PICKER, result);
+    }
+
+    @Test
+    public void given_a_date_input_then_livedata_should_expose_viewstate_with_given_date() {
+        // Given
+        viewModel.onDateChanged(DEFAULT_DATE.getYear(), DEFAULT_DATE.getMonthValue(), DEFAULT_DATE.getDayOfMonth());
 
         // When
         DateFilterViewState result = LiveDataTestUtil.getValueForTesting(viewModel.getViewStateMutableLiveData());
 
         // Then
-        assertEquals(getExpectedDateFilterViewState(DATE, START_TIME, END_TIME), result);
+        assertEquals(getExpectedDateFilterViewState(DEFAULT_DATE, null, null), result);
+    }
+
+    @Test
+    public void given_a_start_time_input_then_livedata_should_expose_viewstate_with_given_start_time() {
+        // Given
+        viewModel.onStartTimeChanged(DEFAULT_START_TIME.getHour(), DEFAULT_START_TIME.getMinute());
+
+        // When
+        DateFilterViewState result = LiveDataTestUtil.getValueForTesting(viewModel.getViewStateMutableLiveData());
+
+        // Then
+        assertEquals(getExpectedDateFilterViewState(null, DEFAULT_START_TIME, null), result);
+    }
+
+    @Test
+    public void given_an_end_time_input_then_livedata_should_expose_viewstate_with_given_end_time() {
+        // Given
+        viewModel.onEndTimeChanged(DEFAULT_END_TIME.getHour(), DEFAULT_END_TIME.getMinute());
+
+        // When
+        DateFilterViewState result = LiveDataTestUtil.getValueForTesting(viewModel.getViewStateMutableLiveData());
+
+        // Then
+        assertEquals(getExpectedDateFilterViewState(null, null, DEFAULT_END_TIME), result);
+    }
+
+    @Test
+    public void given_all_inputs_are_filled_livedata_should_expose_viewstate_with_inputted_values() {
+        // Given
+        viewModel.onDateChanged(DEFAULT_DATE.getYear(), DEFAULT_DATE.getMonthValue(), DEFAULT_DATE.getDayOfMonth());
+        viewModel.onStartTimeChanged(DEFAULT_START_TIME.getHour(), DEFAULT_START_TIME.getMinute());
+        viewModel.onEndTimeChanged(DEFAULT_END_TIME.getHour(), DEFAULT_END_TIME.getMinute());
+
+        // When
+        DateFilterViewState result = LiveDataTestUtil.getValueForTesting(viewModel.getViewStateMutableLiveData());
+
+        // Then
+        assertEquals(getExpectedDateFilterViewState(DEFAULT_DATE, DEFAULT_START_TIME, DEFAULT_END_TIME), result);
+    }
+
+    @Test
+    public void given_on_clear_date_filter_called_livedata_should_expose_viewstate_with_null_date() {
+        // Given
+        viewModel.onDateChanged(DEFAULT_DATE.getYear(), DEFAULT_DATE.getMonthValue(), DEFAULT_DATE.getDayOfMonth());
+
+        // When
+        viewModel.onClearDateFilter();
+        DateFilterViewState result = LiveDataTestUtil.getValueForTesting(viewModel.getViewStateMutableLiveData());
+
+        // Then
+        assertEquals(getExpectedDateFilterViewState(null, null, null), result);
+    }
+
+    @Test
+    public void given_on_clear_start_time_filter_called_livedata_should_expose_viewstate_with_null_start_time() {
+        // Given
+        viewModel.onStartTimeChanged(DEFAULT_START_TIME.getHour(), DEFAULT_START_TIME.getMinute());
+
+        // When
+        viewModel.onClearStartTimeFilter();
+        DateFilterViewState result = LiveDataTestUtil.getValueForTesting(viewModel.getViewStateMutableLiveData());
+
+        // Then
+        assertEquals(getExpectedDateFilterViewState(null, null, null), result);
+    }
+
+    @Test
+    public void given_on_clear_end_time_filter_called_livedata_should_expose_viewstate_with_null_end_time() {
+        // Given
+        viewModel.onEndTimeChanged(DEFAULT_END_TIME.getHour(), DEFAULT_END_TIME.getMinute());
+
+        // When
+        viewModel.onClearEndTimeFilter();
+        DateFilterViewState result = LiveDataTestUtil.getValueForTesting(viewModel.getViewStateMutableLiveData());
+
+        // Then
+        assertEquals(getExpectedDateFilterViewState(null, null, null), result);
     }
 
     // region OUT
