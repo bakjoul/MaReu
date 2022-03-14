e370440cc65bb0419fec8b10c29060febd7d93bd
bakjoul <bakjoul@gmail.com>
2022-03-14 22:06:51 +0100

    Added some UI tests, writing filters tests

---
+++ app/src/androidTest/java/com/bakjoul/mareu/HiltAndroidTestRunner.java
@@ -0,0 +1,18 @@
+package com.bakjoul.mareu;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.test.runner.AndroidJUnitRunner;
+
+import dagger.hilt.android.testing.HiltTestApplication;
+
+@SuppressWarnings("unused")
+public class HiltAndroidTestRunner extends AndroidJUnitRunner {
+
+    @Override
+    public Application newApplication(ClassLoader cl, String className, Context context)
+            throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        return super.newApplication(cl, HiltTestApplication.class.getName(), context);
+    }
+}
--- app/src/androidTest/java/com/bakjoul/mareu/MeetingActivityTest.java
+++ app/src/androidTest/java/com/bakjoul/mareu/MeetingActivityTest.java
@@ -2,11 +2,14 @@ package com.bakjoul.mareu;
 
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
+import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -22,25 +25,40 @@ import static org.hamcrest.CoreMatchers.is;
 
 import androidx.annotation.NonNull;
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.bakjoul.mareu.data.BuildConfigResolver;
 import com.bakjoul.mareu.data.model.Room;
 import com.bakjoul.mareu.ui.MeetingActivity;
+import com.bakjoul.mareu.utils.ClickChildViewWithId;
 import com.bakjoul.mareu.utils.MaterialPickerActions;
+import com.bakjoul.mareu.utils.RecyclerViewItemAssertion;
+import com.bakjoul.mareu.utils.RecyclerViewItemCountAssertion;
 import com.wdullaer.materialdatetimepicker.date.DayPickerView;
 import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
 
-import org.junit.After;
-import org.junit.Before;
+import org.apache.commons.lang3.StringUtils;
+import org.hamcrest.CoreMatchers;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+
+import dagger.hilt.android.testing.BindValue;
+import dagger.hilt.android.testing.HiltAndroidRule;
+import dagger.hilt.android.testing.HiltAndroidTest;
 
 @RunWith(AndroidJUnit4.class)
+@HiltAndroidTest
 public class MeetingActivityTest {
 
     // region CONSTANTS
@@ -96,30 +114,84 @@ public class MeetingActivityTest {
     private static final List<String> SEVENTH_PARTICIPANTS = Arrays.asList("maxime@lamzone.com", "alex@lamzone.com");
     // endregion
 
-    private MeetingActivity meetingActivity;
+    @Rule
+    public HiltAndroidRule hiltAndroidRule = new HiltAndroidRule(this);
 
-    @Before
-    public void setUp() {
-        ActivityScenario<MeetingActivity> activityScenario = ActivityScenario.launch(MeetingActivity.class);
-        activityScenario.onActivity(activity -> meetingActivity = activity);
-    }
+    @BindValue
+    BuildConfigResolver buildConfigResolver = Mockito.mock(BuildConfigResolver.class);
 
-    @After
-    public void tearDown() {
-        meetingActivity = null;
+    @Test
+    public void meetingsList_shouldNotBeEmpty() {
+        Mockito.doReturn(true).when(buildConfigResolver).isDebug();
+        ActivityScenario.launch(MeetingActivity.class);
+
+        onView(withId(R.id.meeting_list)).check(matches(hasMinimumChildCount(1)));
     }
 
     @Test
     public void createOneMeeting() {
-        createMeeting(FIRST_SUBJECT,
-                FIRST_ROOM,
-                FIRST_DATE,
-                FIRST_START,
-                FIRST_END,
-                FIRST_PARTICIPANTS
-        );
+        ActivityScenario.launch(MeetingActivity.class);
+
+        // Crée une réunion
+        createMeeting(FIRST_SUBJECT, FIRST_ROOM, FIRST_DATE, FIRST_START, FIRST_END, FIRST_PARTICIPANTS);
+
+        // Vérifie qu'elle a été créée
+        onView(withId(R.id.meeting_list)).check(new RecyclerViewItemCountAssertion(1));
+        // Vérifie que le contenu correspond à ce qu'on voulait créer
+        assertItemContent(0, FIRST_SUBJECT, FIRST_DATE, FIRST_START, FIRST_END, FIRST_ROOM, FIRST_PARTICIPANTS);
+
+        // Ouvre le dialog de création puis revient
+        onView(withId(R.id.fab_add)).perform(click());
+        pressBack();
+
+        // Revérifications
+        onView(withId(R.id.meeting_list)).check(new RecyclerViewItemCountAssertion(1));
+        assertItemContent(0, FIRST_SUBJECT, FIRST_DATE, FIRST_START, FIRST_END, FIRST_ROOM, FIRST_PARTICIPANTS);
     }
 
+    @Test
+    public void deleteOneMeeting() throws InterruptedException {
+        ActivityScenario.launch(MeetingActivity.class);
+
+        // Action : Crée une première réunion
+        createMeeting(FIRST_SUBJECT, FIRST_ROOM, FIRST_DATE, FIRST_START, FIRST_END, FIRST_PARTICIPANTS);
+        // Assertion : Vérifie qu'elle a été créée
+        onView(withId(R.id.meeting_list)).check(new RecyclerViewItemCountAssertion(1));
+        // Assertion : Vérifie que le contenu correspond à ce qu'on voulait créer
+        assertItemContent(0, FIRST_SUBJECT, FIRST_DATE, FIRST_START, FIRST_END, FIRST_ROOM, FIRST_PARTICIPANTS);
+
+        // Action : Crée une deuxième réunion
+        createMeeting(SECOND_SUBJECT, SECOND_ROOM, SECOND_DATE, SECOND_START, SECOND_END, SECOND_PARTICIPANTS);
+        // Assertion : Vérifie qu'elle a été créée
+        onView(withId(R.id.meeting_list)).check(new RecyclerViewItemCountAssertion(2));
+        // Assertions : Vérifie que les contenus correspondent à ce qu'on voulait créer
+        assertItemContent(0, FIRST_SUBJECT, FIRST_DATE, FIRST_START, FIRST_END, FIRST_ROOM, FIRST_PARTICIPANTS);
+        assertItemContent(1, SECOND_SUBJECT, SECOND_DATE, SECOND_START, SECOND_END, SECOND_ROOM, SECOND_PARTICIPANTS);
+
+        // Action : Supprime la première réunion
+        onView(withId(R.id.meeting_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, new ClickChildViewWithId(R.id.item_delete_button)));
+        Thread.sleep(1000);
+        // Assertion : Vérifie que la réunion a été supprimée
+        onView(withId(R.id.meeting_list)).check(new RecyclerViewItemCountAssertion(1));
+        // Assertion : Vérifie que la seconde réunion créée est maintenant en position 0
+        assertItemContent(0, SECOND_SUBJECT, SECOND_DATE, SECOND_START, SECOND_END, SECOND_ROOM, SECOND_PARTICIPANTS);
+    }
+
+    @Test
+    public void filterByDate() throws InterruptedException {
+        Mockito.doReturn(true).when(buildConfigResolver).isDebug();
+        ActivityScenario.launch(MeetingActivity.class);
+
+        onView(withId(R.id.menu_filters)).perform(click());
+        onData(CoreMatchers.anything())
+                .inRoot(RootMatchers.isPlatformPopup())
+                .atPosition(0)
+                .perform(click());
+        onView(withId(R.id.filter_room_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        Thread.sleep(3000);
+    }
+
+    // region Utils
     // Crée une réunion
     private void createMeeting(
             @NonNull String subject,
@@ -173,4 +245,50 @@ public class MeetingActivityTest {
         onView(withId(R.id.create_new_meeting)).perform(click());
     }
 
+    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM", Locale.FRENCH);
+    private final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH'h'mm", Locale.FRENCH);
+
+    private String formatDate(@NonNull LocalDate date) {
+        return date.format(dateFormatter);
+    }
+
+    private String formatTime(@NonNull LocalTime time) {
+        return time.format(timeFormatter);
+    }
+
+    private String formatParticipants(@NonNull List<String> participants) {
+        return StringUtils.join(participants, ", ");
+    }
+
+    private void assertItemContent(
+            int position,
+            @NonNull String subject,
+            @NonNull LocalDate date,
+            @NonNull LocalTime start,
+            @NonNull LocalTime end,
+            @NonNull Room room,
+            @NonNull List<String> participants) {
+        onView(withId(R.id.meeting_list)).check(
+                new RecyclerViewItemAssertion(
+                        position,
+                        R.id.item_subject,
+                        withText(subject)
+                )
+        );
+        onView(withId(R.id.meeting_list)).check(
+                new RecyclerViewItemAssertion(
+                        position,
+                        R.id.item_info,
+                        withText(formatDate(date) + " – " + formatTime(start) + "-" + formatTime(end) + " – " + room)
+                )
+        );
+        onView(withId(R.id.meeting_list)).check(
+                new RecyclerViewItemAssertion(
+                        position,
+                        R.id.item_participants,
+                        withText(formatParticipants(participants))
+                )
+        );
+    }
+    // endregion
 }
